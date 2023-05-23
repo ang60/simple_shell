bc3a6ac44ada5f573419df85fb538573a07769dc
angie <angelngina98@gmail.com>
2023-05-23 10:12:46 +0300

    code

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define COMMAND_COUNT 10
+
+void turtleShell(char av, char env);
+
+#endif
