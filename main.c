048ebf4ce6aaf3aa2f62dde43c6d85a4fba81778
angie <angelngina98@gmail.com>
2023-05-26 22:02:00 +0300

    changes to main.c file

--- main.c
+++ main.c
@@ -1,18 +1,50 @@
 #include "main.h"
 
 /**
-* main - main function
-* @ac: - argument counter
-* @av: - argument vector
-* @env: - enviromental variable
-* Return: 0 - success.
-*/
+ * main - main function
+ *
+ * @ac:  arguments
+ * @argv: arguments array
+ * Return: 0 success
+ */
 
-int main(int ac, char **av, char **env)
+int main(int ac, char **argv)
 {
-	if (ac == 1)
-	{
-		turtleShell(av, env);
-	}
-	return (0);
+    char *cmdptr, **args;
+    size_t cmdlen = 0;
+    ssize_t getline_result;
+    int i;
+
+    while (1)
+    {
+        printf("%s", "$ ");
+        getline_result = getline(&cmdptr, &cmdlen, stdin);
+        if (getline_result == -1)
+        {
+            printf("Exit..\n");
+            return (-1);
+        }
+
+        args = stringTokenizer(cmdptr);
+
+        if (args == NULL)
+        {
+            continue;
+        }
+
+        execmd(args);
+
+        for (i = 0; args[i] != NULL; i++)
+        {
+            free(args[i]);
+        }
+        free(args);
+
+        free(cmdptr);
+        cmdptr = NULL;
+        cmdlen = 0;
+    }
+    return (0);
+    ac++;
+    argv[i] = "l";
 }
